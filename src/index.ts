f0ecb49132191e2b0469eb108c9ee71b5ae74d87
Tang Bo Hao <tech@btang.cn>
2021-07-02 23:09:42 +0800

    add koa service and routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import Logger from '@jadepool/logger'
 import jadepool from '@jadepool/instance'
-// import * as consts from '@jadepool/consts'
+import consts from '@jadepool/consts'
+import { KoaService } from '@jadepool/service-http'
+
 import Context from './context'
+import router from './routes'
 
 const logger = Logger.of('Mintcraft', 'NFT gateway')
 
 async function run (): Promise<void> {
   await jadepool.initialize(new Context())
-  // const serviceKey = jadepool.env.processKey
+
   logger.tag('Context Built').logObj(jadepool.env)
+  // http 服务
+  const appSrv: KoaService = (await jadepool.registerService(consts.SERVICE_NAMES.KOA, {
+    listenManually: true,
+    router: router
+  })) as KoaService
+
+  // socket.io服务
+  await jadepool.registerService(consts.SERVICE_NAMES.SOCKET_IO)
+
+  // 启动listen
+  await appSrv.listen()
 }
 
 // eslint-disable-next-line no-void
--- src/routes/v0/index.ts
+++ src/routes/v0/index.ts
@@ -1,5 +1,10 @@
 import Router from '@koa/router'
+// routers
+import routerNFT from './nft'
+import routerStorage from './storage'
 
 const router = new Router()
+router.use('/nft', routerNFT.routes())
+router.use('/storage', routerStorage.routes())
 
 export = router
